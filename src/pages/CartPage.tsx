d264de81474c85344dbb894ed4e9037cb2ec1d02
esisramadhan <esis.ramadhan@gmail.com>
2024-08-27 10:16:01 +0700

    add cartpage

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -8,6 +8,8 @@ const CartPage: React.FC = () => {
     const quantity = parseInt(event.target.value, 10)
     if (!isNaN(quantity) && quantity > 0) {
       updateQuantity(id, quantity)
+    } else {
+      updateQuantity(id, 1) // Default to 1 if the input is invalid
     }
   }
 
@@ -19,11 +21,19 @@ const CartPage: React.FC = () => {
       ) : (
         <ul>
           {cart.map((item) => (
-            <li key={item.id}>
+            <li key={item.id} style={{ marginBottom: '20px' }}>
               <h2>{item.title}</h2>
               <p>Price: ${item.price}</p>
-              <input type='number' value={item.quantity} onChange={(e) => handleQuantityChange(item.id, e)} />
-              <button onClick={() => removeFromCart(item.id)}>Remove</button>
+              <input
+                type='number'
+                value={item.quantity}
+                min='1'
+                onChange={(e) => handleQuantityChange(item.id, e)}
+                style={{ width: '50px', marginRight: '10px' }}
+              />
+              <button onClick={() => removeFromCart(item.id)} style={{ backgroundColor: 'red', color: 'white' }}>
+                Remove
+              </button>
             </li>
           ))}
         </ul>
